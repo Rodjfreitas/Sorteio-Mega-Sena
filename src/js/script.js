8a1346a35d703abc48c80aeb60e6f564bbc599d7
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-13 15:48:28 -0300

    execução e finalização do código que transfere os valores sorteados nas arrays para uma lista li no html.

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,7 @@ const botaoReset = document.querySelector('.reiniciarbtn');
 var sorteio;
 var mega = new Array();
 var list = document.createElement('ul');
-var item = document.createElement('li');
+
 
 
 
@@ -27,17 +27,22 @@ function clicarReset(){
 botaoReset.addEventListener('click',clicarReset);
 
 
+
+
 //Sorteio da Mega//
+
 
 var i = 0;
 while( i < 6){
 
     sorteio = Math.ceil(Math.random()*60);     
-    if(mega.indexOf(sorteio) < 0){        
-        mega.push(sorteio);                             
-        i++; 
-                     
+    if(mega.indexOf(sorteio) < 0){         
+        mega.push(sorteio);                                                
+        i++;
     }  
+    
+     
+    
 
 function ordenarNumeros(a,b){
     return a-b;
@@ -47,16 +52,23 @@ mega.sort(ordenarNumeros);
 console.log(mega);
 
 
+
+
+mega.myUcase = function() {
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i].toUpperCase();
+    }
+  };
+
 //Transferência de informações do Array para Li//
 
 const addLi = function(array){
 
 
     for(var i = 0; i < array.length; i++){
-        
-        list.appendChild(item); 
-        item.appendChild(document.createTextNode(mega[i]));
-        
+        var item = document.createElement('li');
+        item.appendChild(document.createTextNode(array[i])); 
+        list.appendChild(item);
                
     }
     
@@ -68,9 +80,6 @@ const addLi = function(array){
 
 
 document.getElementById('mostrar-lista').appendChild(addLi(mega));
-
-
-
 
 
 
