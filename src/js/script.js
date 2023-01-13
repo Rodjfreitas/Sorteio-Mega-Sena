6f361240cd64877cf94e12a44509afb3fe29075a
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-13 15:57:20 -0300

    limpando o código js e excluindo códigos não utilizados no arquivo

--- src/js/script.js
+++ src/js/script.js
@@ -9,16 +9,16 @@ var list = document.createElement('ul');
 
 
 
-
+//Mudar de página ao clicar
 function clicarSorteio(){    
     paginaPrincipal.style.display = 'none';
     paginaSorteio.style.display = 'block';
 }
 
-
+//Evento do Clique
 botaoSorteio.addEventListener('click',clicarSorteio);
 
-
+//Resetar a página para que possa sortear novos números no próximo evento de clique
 function clicarReset(){
     location.reload();
 
@@ -30,55 +30,30 @@ botaoReset.addEventListener('click',clicarReset);
 
 
 //Sorteio da Mega//
-
-
 var i = 0;
 while( i < 6){
-
     sorteio = Math.ceil(Math.random()*60);     
     if(mega.indexOf(sorteio) < 0){         
         mega.push(sorteio);                                                
         i++;
-    }  
-    
-     
-    
-
+    }
 function ordenarNumeros(a,b){
     return a-b;
 }
 mega.sort(ordenarNumeros);
 }
-console.log(mega);
 
 
-
-
-mega.myUcase = function() {
-    for (let i = 0; i < this.length; i++) {
-      this[i] = this[i].toUpperCase();
-    }
-  };
 
 //Transferência de informações do Array para Li//
-
 const addLi = function(array){
-
-
     for(var i = 0; i < array.length; i++){
         var item = document.createElement('li');
         item.appendChild(document.createTextNode(array[i])); 
-        list.appendChild(item);
-               
-    }
-    
-    console.log(list);    
-    return list;
-    
+        list.appendChild(item);               
+    }    
+    return list;    
 }
-
-
-
 document.getElementById('mostrar-lista').appendChild(addLi(mega));
 
 
