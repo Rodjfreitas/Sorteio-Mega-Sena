5f7c34429de16fcf2af6a2e3076024081d858ed3
Rodjfreitas <rodrigofreitas2011@live.com>
2023-01-13 00:34:49 -0300

    confecção do código js para sorteio dos números e apresentação na tela. reestruturação da div onde esta localizada a lista

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,10 @@ const paginaPrincipal = document.querySelector('.carregamento');
 const paginaSorteio = document.querySelector('.sorteio');
 const botaoSorteio = document.querySelector('.sortearbtn');
 const botaoReset = document.querySelector('.reiniciarbtn');
+var sorteio;
+var mega = new Array();
+var list = document.createElement('ul');
+var item = document.createElement('li');
 
 
 
@@ -21,3 +25,49 @@ function clicarReset(){
 }
 
 botaoReset.addEventListener('click',clicarReset);
+
+
+//Sorteio da Mega//
+
+var i = 0;
+while( i < 6){
+
+    sorteio = Math.ceil(Math.random()*60);     
+    if(mega.indexOf(sorteio) < 0){        
+        mega.push(sorteio);                       
+        i++; 
+                     
+    }  
+
+function ordenarNumeros(a,b){
+    return a-b;
+}
+mega.sort(ordenarNumeros);
+}
+console.log(mega);
+
+
+
+
+const addLi = function(array){
+
+
+    for(var i = 0; i < array.length; i++){
+        
+
+        item.appendChild(document.createTextNode("numero: "+ sorteio.indexOf(array[i])));
+        list.appendChild(item);        
+    }
+    console.log(list);    
+    return list;
+    
+}
+
+document.getElementById('mostrar-lista').appendChild(addLi(sorteio));
+
+
+
+
+
+
+
